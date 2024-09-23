ed5f6bd4b7f7f77a975dfd941ed098e162a3824d
Apoorva Viresh D <119603997+apoorvaaxo@users.noreply.github.com>
2024-09-23 07:30:50 +0000

    datatypesummary  added

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,41 @@
+/* 
+=>primitive data types (7 categories)
+1. string
+2. number
+    const score=99
+3. bigint
+const bigNumber=2345678765432n 
+4. null
+    const outsideTemp=null
+
+5. undefined
+    let useEmail;
+6. boolean
+ const isLoggedIn=false
+
+7. symbol
+const id=Symbol('123')
+const anotherId=Symbol=('123')
+
+=>non primitive data type or reference( 3 categories)
+1. object
+let myobj ={
+name:"apoorva",
+age:"21",
+}
+
+2. array
+const hero=["shaktiman" , "chota bheem" , "doga"]
+
+3.  functions
+
+const function(){
+console.log("hello");
+}
+
+=>Is javascript dynamically typed or statically typed?
+it is dynamically typed  because it allows you to initialise a variable
+to a data type and then reassign it to a different data type
+
+
+*/
