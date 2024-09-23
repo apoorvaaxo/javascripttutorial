884704f14adcc9f5f41d43e859e91f583a4e4aa7
Apoorva Viresh D <119603997+apoorvaaxo@users.noreply.github.com>
2024-09-23 07:11:43 +0000

    04  added

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(1<2)
+// console.log(1>2)
+// console.log(2<=2)
+// console.log(2==2)
+
+//make sure its the same data type to avoid confusion
+console.log("02" > 1)
+console.log("2" > 1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+
+// === strict check 
+
+console.log("2"===2)
+console.log(2===2)
